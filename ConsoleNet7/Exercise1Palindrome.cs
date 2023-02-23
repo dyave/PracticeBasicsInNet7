e1e7f5297ae75399e82ff8f7a0e167ad4f3dcf0d
Diego Andres Yave Guzmain <di.an.yave@gmail.com>
2023-02-23 09:57:01 +0100

    Exercise #1 Palindrome

---
+++ ConsoleNet7/Exercise1Palindrome.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ConsoleNet7;
+
+public static class Exercise1
+{
+	public static void Palindrome()
+	{
+        Random rnd = new();
+        bool isPal = false;
+
+        while (!isPal)
+        {
+            int num = rnd.Next(0, 9999);
+            Console.WriteLine(num);
+            isPal = CheckPalindrome(num);
+        }
+
+        Console.WriteLine("{0} Palindrome", isPal ? "Is" : "Isn't");
+    }
+
+
+    static bool CheckPalindrome(int num)
+    {
+        //int[] digits = new int[];
+        List<int> digits = new();
+
+        int cociente = num;
+        do
+        {
+            int res = cociente % 10;
+            digits.Add(res);
+            //Console.WriteLine(res);
+            cociente = cociente / 10;
+        }
+        while (cociente != 0);
+
+
+        digits.Count();
+        int firstDigit = digits.ElementAt(0);
+        int size = digits.Count();
+
+
+        for (int i = 0; i < size - 1; i++)
+        {
+            if (digits.ElementAt(i) != digits.ElementAt(size - 1 - i))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+
+
